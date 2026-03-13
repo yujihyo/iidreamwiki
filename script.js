6b9d45739632db630432396e504144bf5c1d53a0
yujihyo <rgh822@naver.com>
2026-03-13 17:45:47 +0900

    5

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("IIDRWIKI loaded");
+document.querySelectorAll("h2").forEach(function(title){
+
+let anchor = document.createElement("a");
+
+anchor.href = "#" + title.id;
+
+anchor.textContent = " #";
+
+anchor.style.color = "#999";
+anchor.style.fontSize = "14px";
+anchor.style.textDecoration = "none";
+
+title.appendChild(anchor);
+
+});
